6e1acb0b20cde2308ca42f4b6e9ef40192ce8573
paranoidbarbie <brown.hannah2024@gmail.com>
2024-06-20 19:31:03 +0100

    Hello command

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+from os import getenv
+
+#load dotenv & assign variable to it
+load_dotenv()
+TOKEN = getenv('SERVER_TOKEN')
+
+#bot starts here
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+@bot.event
+async def on_ready():
+    print("We're ready master.")
+
+@bot.command()
+async def hello(self):
+    await self.send(f"Hi cutie >_< {self.author.mention}.")
+
+
+
+
+bot.run(TOKEN)
